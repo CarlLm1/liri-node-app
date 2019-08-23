be3da3b36e5a1f65558f4b636a07d1b07ee772ee
Carl Medina <carl.l.medina@comcast.net>
2019-08-22 20:14:23 -0700

    stored files into variables on liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,6 @@
-// 996fe45e
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var inquirer = require("inquirer");
+var moment = require("moment");
